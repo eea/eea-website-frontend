05ad4984bb152b47532ccfb290cf58e7d65569c6
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-05-12 16:15:06 +0300

    Fix start script

--- start_dev.sh
+++ start_dev.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 env \
   DEBUG=esmiddleware \
+  RAZZLE_PROXY_ES_DSN_datahub=http://10.120.10.204:60168/data_searchui_datahub \
+  RAZZLE_PROXY_ES_DSN_globalsearch=http://10.120.10.131:57664/data_searchui_datahub \
   RAZZLE_PROXY_QA_DSN_globalsearch=http://localhost:8000/api \
-  RAZZLE_PROXY_ES_DSN_datahub=http://10.120.10.204:60168/data_searchui_datahub yarn start
-  RAZZLE_PROXY_ES_DSN_globalsearch=http://10.120.10.131:57664/data_searchui_datahub yarn start
+  yarn start
