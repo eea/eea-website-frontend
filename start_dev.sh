9709c6caa9c19c7eb3db34050237bf4d7131cb83
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-05-12 12:55:08 +0300

    Don't need this line in start script

--- start_dev.sh
+++ start_dev.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 env \
-  UV_THREADPOOL_SIZE=10 \
   DEBUG=esmiddleware \
   RAZZLE_PROXY_QA_DSN=http://localhost:8000/api \
   RAZZLE_PROXY_ES_DSN_datahub=http://10.120.10.204:60168/data_searchui_datahub yarn start
