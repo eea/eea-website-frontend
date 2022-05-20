e493aa759d24e2307b7e1569e7ef876d18746ec8
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-05-20 10:55:54 +0300

    Use production ES8 + nlpservice ES8 setup

--- start_dev.sh
+++ start_dev.sh
@@ -2,6 +2,9 @@
 env \
   DEBUG=esmiddleware \
   RAZZLE_PROXY_ES_DSN_datahub=http://10.120.10.204:60168/data_searchui_datahub \
-  RAZZLE_PROXY_ES_DSN_globalsearch=http://10.120.10.131:57664/data_searchui_datahub \
-  RAZZLE_PROXY_QA_DSN_globalsearch=http://localhost:8000/api \
+  RAZZLE_PROXY_ES_DSN_globalsearch=http://10.120.10.204:60168/data_searchui \
+  RAZZLE_PROXY_QA_DSN_globalsearch=http://10.120.10.204:60022/api \
   yarn start
+
+# RAZZLE_PROXY_ES_DSN_datahub=http://10.120.10.131:57664/data_searchui_datahub \
+# RAZZLE_PROXY_QA_DSN_globalsearch=http://localhost:8000/api \
