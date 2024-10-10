7142abc8232134b585ff51c3eece29b53f9da0e8
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-10-10 13:35:36 +0300

    Update upgrade_content.cy.js

--- scripts/upgrade_content_cy/cypress/e2e/upgrade_content.cy.js
+++ scripts/upgrade_content_cy/cypress/e2e/upgrade_content.cy.js
@@ -7,30 +7,24 @@ describe('Upgrade content', () => {
     const contentTypes = Cypress.env('contentTypes');
     const LOGIN_ROUTE = Cypress.env('loginRoute');
     let shouldWaitAndRetry = false;
-
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false;
     });
-
     const checkAndSave = () => {
-        
-
         cy.wrap(null).then(() => {
             cy.log("Checking if retry is needed...", shouldWaitAndRetry);
             cy.wait(1000);
             cy.get('body').then(($body) => {
                 if ($body.find('#toolbar-save').length === 0) {
                     cy.log("Edit button not found, save successful!");
-
-                    shouldWaitAndRetry = false; 
-
+                    shouldWaitAndRetry = false;
                 } else {
                     cy.log("Edit button still present, retrying...");
                     if (shouldWaitAndRetry) {
                         cy.log("Retrying save in 5 seconds...");
-                        cy.get('#toolbar-save').click({ force: true }); 
-                        cy.wait(5000); 
-                        checkAndSave(); 
+                        cy.get('#toolbar-save').click({ force: true });
+                        cy.wait(5000);
+                        checkAndSave();
                     } else {
                         cy.log("Content saved successfully, moving to the next link...");
                     }
@@ -41,20 +35,15 @@ describe('Upgrade content', () => {
     };
 
     it('should edit+save all content types listed', () => {
-
         cy.intercept('*').as('allRequests');
-
-        // Proces de login
-        cy.visit(URL + "/logout"); 
+        cy.visit(URL + "/logout");
         cy.visit(URL + LOGIN_ROUTE);
 
         cy.get('#login').type(USERNAME);
         cy.get('#password').type(PASSWORD);
         cy.get('#login-form-submit').click();
 
-        cy.wait(5000); 
-
-   
+        cy.wait(5000);
         if (multiLingual) {
             URL += '/en';
         }
@@ -68,8 +57,7 @@ describe('Upgrade content', () => {
         cy.get('.querystring-widget').get('.fields').click();
         cy.get('.fields').contains('div', 'Type').click();
 
-        cy.on('window:alert', (alertText) => {
-
+      cy.on('window:alert', (alertText) => {
             if (alertText.includes('Wait')) {
                 shouldWaitAndRetry = true;
 
@@ -77,6 +65,10 @@ describe('Upgrade content', () => {
                 shouldWaitAndRetry = false;
             }
         });
+
+        cy.get('#field-listingBodyTemplate').click();
+        cy.contains('div', 'Listing').click();
+        cy.get('input[name="field-hasLink-1-itemModel"]').click({ force: true });
         contentTypes.forEach((contentTypeName) => {
             cy.get('.querystring-widget .fields .field')
                 .eq(2)
@@ -84,33 +76,45 @@ describe('Upgrade content', () => {
                 .then(($el) => {
                     const width = $el.width();
                     const height = $el.height();
-                    cy.wrap($el).click(width - 1, height - 1); 
+                    cy.wrap($el).click(width - 1, height - 1);
                 });
+
             cy.get('.fields').contains('div', contentTypeName).click();
-            cy.wait(2000); 
+            cy.wait(2000);
         });
 
-    
+        let resultsNumber = 1000;
+        cy.get('h4.search-details')
+            .invoke('text')
+            .then((text) => {
+                resultsNumber = text.match(/\d+/)[0];
+                cy.get('#field-b_size-4-query').type(resultsNumber)
+                cy.wait(5000);
+            });
         const hrefs = [];
         cy.get('#toolbar-save').click();
-        cy.wait(1000); 
+        cy.wait(1000);
 
-        cy.get('.listing-item a')
-            .each(($el) => {
-                const href = $el.attr('href');
-                hrefs.push(href); 
-            })
+        cy.get('.ui.fluid.card.u-card').each(($el) => {
+            const img = $el.find('img');
+            if (img.attr('src') === '/static/media/default-image.28e2db6d.svg') {
+
+                const link = $el.find('a').attr('href');
+                if (link) {
+                    hrefs.push(link);
+                }
+            }
+        })
             .then(() => {
-                cy.log('Toate href-urile: ', hrefs);
                 hrefs.forEach((link) => {
                     const editUrl = URL + link + "/edit";
                     cy.visit(editUrl);
                     cy.intercept('*').as('allRequestsInPage');
                     cy.wait('@allRequestsInPage');
-                    cy.wait(5000); 
-                    cy.get('#toolbar-save').click({ force: true }); 
-                    checkAndSave(); 
-                    cy.wait(3000); 
+                    cy.wait(5000);
+                    cy.get('#toolbar-save').click({ force: true });
+                    checkAndSave();
+                    cy.wait(3000);
                 });
             });
     });
